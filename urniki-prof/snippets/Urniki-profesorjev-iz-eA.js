3262642812ec7516f80a8e8381d967594c14029e
zznidar <zznidar@users.noreply.github.com>
2019-11-07 15:06:14 +0100

    Support multiple classes in one room at a time, add classname

--- urniki-prof/snippets/Urniki-profesorjev-iz-eA.js
+++ urniki-prof/snippets/Urniki-profesorjev-iz-eA.js
@@ -1,5 +1,5 @@
 // Snippet za zajem urnikov profesorjev s spletne strani urnikov eAsistent
-// Nazadnje posodobljeno: 2019-10-22
+// Nazadnje posodobljeno: 2019-11-07
 /* Avtor: Žan Žnidar */
 
 const projekt = "https://www.easistent.com/urniki/56149a5b3eb5cbefdaf9c9ded39a80c8c31d8f33/ucilnice/652150"; // Main link, ki pelje do urnika sole -> ucilnice -> prva ucilnica, za trenutni teden/urnik
@@ -12,6 +12,8 @@ ampak moramo potem pocakati, da se nalaganje (NE onload iframa) konca. */
 
 
 o = {}; // Objekt s profesorji   v obliki {"Ime Priimek": [pon[1...10],tor[1...10]...], "Ime2 Priimek2": [...]}   , zapisane so ucilnice
+o[""] = []; // Dodamo se praznega profesorja, da ni errorja, dokler ni izbran noben profesor pri prikazu
+console.log("Dodan prazen profesor.");
 
 
 // Ustvarimo iFrame, znotraj katerega bomo pregledvali ucilnice (s tem se izognemo prenehanju izvajanja skripta ob osvezitvi/navigaciji/menjavi uclinice)
@@ -42,25 +44,28 @@ function check() {
 	
 			if(celice[i].getElementsByClassName("text11").length !== 0) { // Ce je ura polna/zasedena ;; Predpostavimo, da v urniku ni napak (da bi bilo vec profesorjev hkrati v isti uclinici)
 				
-			var hkrativeniuri = []; // Array, kjer preverimo, da nima vec prof. hkrati v isti ucilnici
-			for (duple = 0; duple < celice[i].getElementsByClassName("text11").length; duple++) {
-				hkrativeniuri.push(celice[i].getElementsByClassName("text11")[duple].innerText.trim());
-			}
-			var hkrativeniuridedupl = [...new Set([...hkrativeniuri])];
-			if (hkrativeniuridedupl.length > 1) {
-				console.warn(hkrativeniuri, i, trenutnaUclinica); 
-			}
-					
-					
-				trenutniProfesor = celice[i].getElementsByClassName("text11")[0].innerText;
-				console.log(i, i%6, i%6-1, Math.floor(i/6+1-1)), trenutniProfesor;
-			
-				if(o[trenutniProfesor] === undefined) {
-					o[trenutniProfesor] = [[[],[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[],[]]]; // Profesor se ne obstaja, ustvarimo array zanj
+				var hkrativeniuri = []; // Array, kjer preverimo, da nima vec prof. hkrati v isti ucilnici
+				for (duple = 0; duple < celice[i].querySelectorAll(".text11:not(.gray)").length; duple++) {
+					hkrativeniuri.push(celice[i].querySelectorAll(".text11:not(.gray)")[duple].innerText.trim());
+				}
+				var hkrativeniuridedupl = [...new Set([...hkrativeniuri])];
+				if (hkrativeniuridedupl.length > 1) {
+					console.warn(hkrativeniuri, i, trenutnaUclinica); 
 				}
 				
-				o[trenutniProfesor][i%6-1][Math.floor(i/6+1-1)].push(trenutnaUclinica); // profesor[dan][ura] 0-based
-			
+				for(var p = 0; p < hkrativeniuridedupl.length; p++) { 
+						
+					trenutniProfesor = hkrativeniuridedupl[p];
+					trenutniRazred = celice[i].getElementsByClassName("text14 bold")[p].innerText; // Predpostavimo, da ce ze je napaka (vec profesorjev hkrati v isti ucilnici), se to ne zgodi, kadar so jeziki.
+					console.log(i, i%6, i%6-1, Math.floor(i/6+1-1)), trenutniProfesor, trenutniRazred;
+				
+					if(o[trenutniProfesor] === undefined) {
+						o[trenutniProfesor] = [[[],[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[],[]]]; // Profesor se ne obstaja, ustvarimo array zanj
+					}
+					
+					o[trenutniProfesor][i%6-1][Math.floor(i/6+1-1)].push(trenutnaUclinica + " (" + trenutniRazred + ")"); // profesor[dan][ura] 0-based   /// 18 (1.A)   * Opomba: pozvizgamo se na pravopisne popravke in zaradi preglednosti pustimo izvoren zapis razreda.
+				
+				}
 				
 			}
 		} 
@@ -71,7 +76,7 @@ function check() {
 		a.selectedIndex += 1;
 		window.frames[0].spremeni_urnik();
 	} else { // Prebili smo se skozi vse ucilnice
-		console.log("Koncano!");
+		console.log("Koncano!");	
 		console.log(o);
 		console.log("Nic ne sortiramo ... :(");
 		
