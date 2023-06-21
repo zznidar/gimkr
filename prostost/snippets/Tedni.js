d18e1f757aa404c65a0f58cc5219064af148e0d8
zznidar <zznidar@users.noreply.github.com>
2023-06-21 10:27:36 +0200

    Update Tedni.js to correctly parse new GimKr website

--- prostost/snippets/Tedni.js
+++ prostost/snippets/Tedni.js
@@ -5,25 +5,30 @@
 tedni = [];
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
-for (var u = 2; u <= 8; u += 2) {
 
-    tabela = document.getElementsByClassName("tableBorder")[0];
-    polja = tabela.getElementsByTagName("td");
-    datumi = polja[u].innerText.split("\n");
-    // 2, 4, 6, 8
+tabela = document.getElementsByClassName("table-wrapper")[0];
+vrste = tabela.getElementsByTagName("tr");
 
-    tedni1 = [];
-    for (var i = 0; i < datumi.length; i++) {
-        tedni1.push(datumi[i].split(" - "));
+for (var v = 1; v < vrste.length; v++) { // 0. vrsta je header
+	vrsta = vrste[v];
+	polje = vrsta.getElementsByTagName("td")[0];
+	datumi = polje.innerText.split("\n");
+	tedni1 = [];
+	for (var i = 0; i < datumi.length; i++) {
+		tedni1.push(datumi[i].split("–"));
 
-        for (var j = 0; j <= 1; j++) {
-            // Številke mesecev zamenjamo za imena mesecev, da jih lahko poparsamo; za končne dodamo minuto pred polnočjo, da se šteje tudi današnji dan; nato jih poparsamo
-            tedni1[i][j] = Date.parse(tedni1[i][j].split(".")[0].trim() + ". " + months[tedni1[i][j].split(".")[1] - 1].trim() + " " + tedni1[i][j].split(".")[2].trim() + (j == 1 ? " 23:59" : "")); // splitamo samo na piko, nato trimmamo, da se izognemo tezavam zaradi typotov (odvecni/manjkajoci presledki pri datumih)
-        }
-    }
+		for (var j = 0; j <= 1; j++) {
+			dd = tedni1[i][j].split(".")[0].trim().padStart(2, "0");
+			mm = tedni1[i][j].split(".")[1].trim().padStart(2, "0");
+			yyyy = tedni1[i][j].split(".")[2].trim();
+			casus = (j == 1 ? "23:59:59" : "00:00:00"); // za končne dodamo minuto pred polnočjo, da se šteje tudi današnji dan, nato jih poparsamo
+			//console.warn(`${yyyy}-${mm}-${dd}T${casus}`)
+			tedni1[i][j] = Date.parse(`${yyyy}-${mm}-${dd}T${casus}`);
+		}
+	}
 
-    console.log(JSON.stringify(tedni1));
-    tedni.push(tedni1);
+	console.log(JSON.stringify(tedni1));
+	tedni.push(tedni1);
 }
 console.log("Array z vsemi tedni:");
 console.log("var tedni = " + JSON.stringify(tedni));
